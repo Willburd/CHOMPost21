2cba946b4535d4f64246fa9d2d7c62c064d53aca
Willburd <7099514+Willburd@users.noreply.github.com>
2024-09-29 22:27:10 -0400

    tgui prettier

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/misc.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/misc.tsx
@@ -84,11 +84,10 @@ export const MessengerEmbeds: FeatureToggle = {
 export const HIDE_PAIN_SCREAM: FeatureToggle = {
   name: 'Hide Pain Scream',
   category: 'GAMEPLAY',
-  description: 
-	'When enabled, stops automatic screaming when damage is taken.',
+  description: 'When enabled, stops automatic screaming when damage is taken.',
   component: CheckboxInput,
 };
-  
+
 export const AutoPunctuation: FeatureToggle = {
   name: 'Automatic Punctuation',
   category: 'GAMEPLAY',
