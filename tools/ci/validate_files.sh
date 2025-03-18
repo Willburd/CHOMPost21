54e890adecc5d327d8938e3505442c20efc8b5b2
Willburd <7099514+Willburd@users.noreply.github.com>
2025-03-18 16:03:36 -0400

    new map system

--- tools/ci/validate_files.sh
+++ tools/ci/validate_files.sh
@@ -21,17 +21,17 @@ if command -v rg >/dev/null 2>&1; then
 	if [ ! rg -P '' >/dev/null 2>&1 ] ; then
 		pcre2_support=0
 	fi
-	code_files="code/**/**.dm modular_chomp/code/**/**.dm" #  CHOMPEdit - Modular code
+	code_files="code/**/**.dm modular_outpost/code/**/**.dm" #  Outpost 21 edit - Modular code
 	map_files="maps/**/**.dmm"
-	modular_map_files="modular_chomp/maps/**/**.dmm" # CHOMPEdit - Modular maps
+	modular_map_files="modular_outpost/maps/**/**.dmm" # Outpost 21 edit - Modular maps
 	# shuttle_map_files="_maps/shuttles/**.dmm"
 	code_x_515="code/**/!(__byond_version_compat).dm"
 else
 	pcre2_support=0
 	grep=grep
-	code_files="-r --include=code/**/**.dm --include=modular_chomp/code/**/**.dm" # CHOMPEdit - Modular code
+	code_files="-r --include=code/**/**.dm --include=modular_outpost/code/**/**.dm" # Outpost 21 edit - Modular code
 	map_files="-r --include=maps/**/**.dmm"
-	modular_map_files="-r --include=modular_chomp/maps/**/**.dmm" # CHOMPEdit - Modular maps
+	modular_map_files="-r --include=modular_outpost/maps/**/**.dmm" # Outpost 21 edit - Modular maps
 	# shuttle_map_files="-r --include=_maps/shuttles/**.dmm"
 	code_x_515="-r --include=code/**/!(__byond_version_compat).dm"
 fi
