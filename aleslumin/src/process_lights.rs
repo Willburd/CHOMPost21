1d25ce04f3d9eaf6c7a04f5f1baff5931b9aba1a
Will <7099514+Willburd@users.noreply.github.com>
2026-09-18 17:50:22 -0400

    verdi removal

--- aleslumin/src/process_lights.rs
+++ aleslumin/src/process_lights.rs
@@ -1,10 +1,8 @@
 use meowtonin::{ByondError, ByondResult, value::ByondValue};
-use rand_distr::{Bernoulli, Distribution};
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 #[byond_fn]
 pub fn generate_automata(
-    input: Vec<ByondValue>,
+    _input: Vec<ByondValue>,
 ) -> ByondResult<Vec<ByondValue>> {
 
     let byond_list: Vec<ByondValue> = Vec::new();
